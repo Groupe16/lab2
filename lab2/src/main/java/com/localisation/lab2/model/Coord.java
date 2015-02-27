8704fe8151e9e0d58716441ece29e8ca7d89b837
Zalila <ay.zalila@gmail.com>
2015-02-27 11:21:05 -0500

    model

--- lab2/src/main/java/com/localisation/lab2/model/Coord.java
+++ lab2/src/main/java/com/localisation/lab2/model/Coord.java
@@ -6,4 +6,28 @@ package main.java.com.localisation.lab2.model;
 public class Coord {
 
     private float longitude, latitude,altitude;
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(float altitude) {
+        this.altitude = altitude;
+    }
 }
--- lab2/src/main/java/com/localisation/lab2/model/PAWifi.java
+++ lab2/src/main/java/com/localisation/lab2/model/PAWifi.java
@@ -3,7 +3,31 @@ package main.java.com.localisation.lab2.model;
 /**
  * Created by Zalila on 2015-02-27.
  */
-public class PAWifi extends PtReseau {
+public class PAWifi {
 
     private String bssid,ssid,rss;
+
+    public String getBssid() {
+        return bssid;
+    }
+
+    public void setBssid(String bssid) {
+        this.bssid = bssid;
+    }
+
+    public String getSsid() {
+        return ssid;
+    }
+
+    public void setSsid(String ssid) {
+        this.ssid = ssid;
+    }
+
+    public String getRss() {
+        return rss;
+    }
+
+    public void setRss(String rss) {
+        this.rss = rss;
+    }
 }
